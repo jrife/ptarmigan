2619bd33962fa22d1172b6980f2de39373e51154
Jordan Rife <jrife0@gmail.com>
2020-03-26 21:16:31 -0500

    Automate protobuf generation

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: proto
+
+proto:
+	docker build -t ptarmigan-protogen -f ./build/Dockerfile.proto .
+	docker run --rm -v /etc/passwd:/etc/passwd -u $(shell id -u):$(shell id -g) -v $(shell pwd):/home/me/ptarmigan ptarmigan-protogen /home/me/ptarmigan/build/generate-proto.sh
---
+++ build/generate-proto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+echo "$(whoami)"
+PTARMIGAN_ROOT=/home/me/ptarmigan
+GOGOPROTO_ROOT=/home/me/protobuf
+cd $PTARMIGAN_ROOT/transport/ptarmiganpb
+protoc -I=$PTARMIGAN_ROOT/vendor/ -I $PTARMIGAN_ROOT -I=$GOGOPROTO_ROOT/protobuf/ -I=$GOGOPROTO_ROOT -I=. --gogofaster_out=paths=source_relative:. ptarmigan.proto
+cd $PTARMIGAN_ROOT/transport/frontends/grpc/pb
+protoc -I=$PTARMIGAN_ROOT/vendor/ -I $PTARMIGAN_ROOT -I=$GOGOPROTO_ROOT/protobuf/ -I=$GOGOPROTO_ROOT -I=. --gogofaster_out=plugins=grpc:. ptarmigan.proto
+cd $PTARMIGAN_ROOT/storage/kv/kvpb
+protoc -I=$PTARMIGAN_ROOT/vendor/ -I $PTARMIGAN_ROOT -I=$GOGOPROTO_ROOT/protobuf/ -I=$GOGOPROTO_ROOT -I=. --gogofaster_out=paths=source_relative:. kv.proto
